e50e344983a1b085ce7ff98105409759455cc307
Rishabh <Rishabh9843@gmail.com>
2020-12-22 11:33:17 +0530

    Added view files for covidapp

--- Covid_Tracker/covidapp/views.py
+++ Covid_Tracker/covidapp/views.py
@@ -13,10 +13,25 @@ response = requests.request("GET", url, headers=headers).json()
 
 
 def helloworldview(request):
-
-    noofresult = int(response['results'])
     mylist = []
+    noofresult = int(response['results'])
     for x in range(noofresult):
         mylist.append(response['response'][x]['country'])
+    if request.method == "POST":
+        selectedcountry = request.POST['selectedcountry']
+        print(selectedcountry)
+        for x in range(0, noofresult):
+            if selectedcountry == response['response'][x]['country']:
+                print(response['response'][x]['cases'])
+                new = response['response'][x]['cases']['new']
+                active = response['response'][x]['cases']['active']
+                critical = response['response'][x]['cases']['critical']
+                recovered = response['response'][x]['cases']['recovered']
+                total = response['response'][x]['cases']['total']
+                deaths = int(total)-int(active)-int(recovered)
+        context = {'mylist': mylist, 'selectedcountry': selectedcountry, 'new': new, 'active': active, 'critical': critical,
+                   "recovered": recovered, 'total': total, "deaths": deaths}
+
+        return render(request, 'helloworld.html', context)
     context = {'mylist': mylist}
     return render(request, 'helloworld.html', context)
